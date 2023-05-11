10cf3bc97fa6b75c6e58e648eca239a84aeb0e50
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-11 22:22:30 +0300

    Ajout de methode update au service

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -1,27 +1,34 @@
 import Pokemon from "../models/pokemon";
 
 export default class PokemonService {
+  static getPokemons(): Promise<Pokemon[]> {
+    return fetch("http://localhost:3001/pokemons")
+      .then((response) => response.json())
+      .catch((error) => this.handleError(error));
+  }
 
-    static getPokemons(): Promise<Pokemon[]>{
-        return fetch('http://localhost:3001/pokemons')
-            .then((response) => response.json())
-            .catch((error) => this.handleError(error))
-        ;
-    }
+  static getPokemon(id: number): Promise<Pokemon | null> {
+    return fetch(`http://localhost:3001/pokemons/${id}`)
+      .then((response) => response.json())
+      .then((data) => (this.isEmpty(data) ? null : data))
+      .catch((error) => this.handleError(error));
+  }
 
-    static getPokemon(id: number): Promise<Pokemon|null>{
-        return fetch(`http://localhost:3001/pokemons/${id}`)
-            .then((response) => response.json())
-            .then(data => this.isEmpty(data) ? null : data)
-            .catch((error) => this.handleError(error))
-        ;
-    }
+  static updatePokemon(pokemon: Pokemon): Promise<Pokemon> {
+    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+      method: "PUT",
+      body: JSON.stringify(pokemon),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .catch((error) => this.handleError(error));
+  }
 
-    static isEmpty(data: Object): boolean{
-        return Object.keys(data).length === 0;
-    }
+  static isEmpty(data: Object): boolean {
+    return Object.keys(data).length === 0;
+  }
 
-    static handleError(error: Error): void{
-        console.error(error);
-    }
+  static handleError(error: Error): void {
+    console.error(error);
+  }
 }
