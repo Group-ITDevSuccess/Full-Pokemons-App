a1d1d0f5e9a96fc3fc66f4fe174dc95ad1fdd534
PRO-ITDevSuccess <itdevsuccess@gmail.com>
2023-05-11 11:56:07 +0300

    Mise en place de propirete des pokemna

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -20,12 +20,56 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
     }
 
     const formatDate = (date: Date): string => {
-        return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    }
+
+    const formatType = (type: string): string => {
+        let color: string;
+
+        switch (type) {
+            case 'Feu':
+                color = 'red lighten-1';
+                break;
+            case 'Eau':
+                color = 'blue lighten-1';
+                break;
+            case 'Plante':
+                color = 'green lighten-1';
+                break;
+            case 'Insecte':
+                color = 'brown lighten-1';
+                break;
+            case 'Normal':
+                color = 'grey lighten-3';
+                break;
+            case 'Vol':
+                color = 'blue lighten-3';
+                break;
+            case 'Poison':
+                color = 'deep-purple accent-1';
+                break;
+            case 'Fée':
+                color = 'pink lighten-4';
+                break;
+            case 'Psy':
+                color = 'deep-purple darken-2';
+                break;
+            case 'Electrik':
+                color = 'lime accent-1';
+                break;
+            case 'Combat':
+                color = 'deep-orange';
+                break;
+            default:
+                color = 'grey';
+                break;
+        }
+        return `chip ${color}`;
     }
 
     return (
         <div className="col s12 m6 l4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
-            <div className="card hoverable" style={{ borderColor: color }} >
+            <div className="card hoverable" style={{ borderColor: color }}>
                 <div className="card-image waves-effect waves-block waves-light">
                     <img className="activator" src={pokemon.picture} alt={pokemon.name} />
                 </div>
@@ -38,7 +82,15 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
                         <small>Created: {formatDate(pokemon.created)}</small>
                     </p>
                 </div>
-                
+                <div className="card-action">
+                    <div className="type-container">
+                        {pokemon.types.map((type) => (
+                            <span className={formatType(type)} key={type}>
+                                {type}
+                            </span>
+                        ))}
+                    </div>
+                </div>
                 <div className="card-reveal">
                     <span className="card-title grey-text text-darken-4">
                         {pokemon.name}
@@ -48,6 +100,7 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
                 </div>
             </div>
         </div>
+
     );
 }
 
