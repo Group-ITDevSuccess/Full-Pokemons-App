be78007cb79ab4bb5f5d77c19b22fec9686a7f02
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-12 19:18:57 +0300

    Ajout de loader

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -3,6 +3,7 @@ import Pokemon from "../models/pokemon";
 import PokemonCard from "../components/pokemon-card";
 import PokemonService from "../services/pokemon-service";
 import { Link } from "react-router-dom";
+import PokemonSearch from "../components/pokemon-search";
 
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -15,6 +16,7 @@ const PokemonList: FunctionComponent = () => {
     <div className="container">
       <h1 className="center">Pokédex</h1>
       <div className="row">
+        <PokemonSearch />
         {pokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} borderColor="red" />
         ))}
